9d34590803cfcd0c4133882d1cfa90d7788a1e22
Le Quyen <lehoangq@gmail.com>
2019-10-17 20:15:46 +0800

    Replicated Apple's GLPaint sample app.
    Change-Id: I33afd93e7b362b8b96abb68a801ce66f3e7faabe

---
+++ ios/xcode/MGLPaint/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  MGLPaint
+//
+//  Created by Le Quyen on 17/10/19.
+//  Copyright © 2019 HQGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ ios/xcode/MGLPaint/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  MGLPaint
+//
+//  Created by Le Quyen on 17/10/19.
+//  Copyright © 2019 HQGame. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
