10e20e824a5facc98d7816fa0ed6316c974d7459
Le Quyen <lehoangq@gmail.com>
2019-10-29 23:07:54 +0800

    Realign comments
    Change-Id: I0236da21f34e08b15da6efde8949ee591c4c22e5

--- src/compiler/translator/TranslatorMetal.cpp
+++ src/compiler/translator/TranslatorMetal.cpp
@@ -8,7 +8,7 @@
 //   The shaders are then fed into glslang to spit out SPIR-V (libANGLE-side).
 //   See: https://www.khronos.org/registry/vulkan/specs/misc/GL_KHR_vulkan_glsl.txt
 //
-//  The SPIR-V will then be translated to Metal Shading Language later in Metal backend.
+//   The SPIR-V will then be translated to Metal Shading Language later in Metal backend.
 //
 
 #include "compiler/translator/TranslatorMetal.h"
