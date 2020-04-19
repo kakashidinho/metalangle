fbd0e96db90b0dcbb10eac7a825720cfe1f089f8
Le Hoang Quyen <lehoangq@gmail.com>
2020-04-20 02:45:47 +0800

    WIP: fix tvOS travis build script
    Change-Id: Iac5c196ef08dbac1dad7a7db58be32623b0b79c1

---
+++ ios/xcode/travis_build_tvos.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CURRENR_DIR=$PWD
+CONFIGURATION=$1
+SDK=$2
+OUTPUT_DIR=$3
+QUIET=$4
+
+if [ "$CONFIGURATION" = "" ]; then
+    CONFIGURATION=Debug
+fi
+
+if [ "$SDK" = "" ]; then
+    SDK=appletvos
+fi
+
+if [ ! "$OUTPUT_DIR" = "" ]; then
+    OUTPUT_DIR_OPTION="-derivedDataPath $OUTPUT_DIR"
+fi
+
+cd $SCRIPT_DIR
+
+invoke_xcodebuild()
+{
+    TARGET=$1
+
+    ERR=0
+    xcodebuild build \
+               -project MGLKitSamples.xcodeproj \
+               -scheme $TARGET  \
+               -sdk $SDK \
+               -configuration $CONFIGURATION \
+               CODE_SIGN_IDENTITY="" \
+               CODE_SIGN_ENTITLEMENTS="" \
+               CODE_SIGNING_REQUIRED=NO \
+               CODE_SIGNING_ALLOWED=NO \
+               $OUTPUT_DIR_OPTION \
+               $QUIET \
+               |  tee xcodebuild.log | xcpretty && ERR=${PIPESTATUS[0]}
+
+    if [ ! "$ERR" = "0" ]; then
+        exit $ERR
+    fi
+}
+
+./fetchDependencies.sh
+
+invoke_xcodebuild MGLKitSampleApp_tvos
+invoke_xcodebuild MGLPaint_tvos
+invoke_xcodebuild simple_vertex_shader_tvos
+
+cd $CURRENR_DIR
+
