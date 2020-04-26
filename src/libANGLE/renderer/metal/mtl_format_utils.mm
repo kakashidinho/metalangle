052ee03c6af2a434851978c12e834fd93cdb483c
Le Hoang Quyen <lehoangq@gmail.com>
2020-04-26 23:47:46 +0800

    Update texture's blendable cap from pixel format's blendable cap.
    This cap was added recently on upstream.
    
    Change-Id: Ic586ff2879956c9b366bd7666b4befb898837d67

--- src/libANGLE/renderer/metal/mtl_format_utils.mm
+++ src/libANGLE/renderer/metal/mtl_format_utils.mm
@@ -77,6 +77,7 @@ void GenerateTextureCapsMap(const FormatTable &formatTable,
             textureCaps.renderbuffer =
                 mtlFormat.getCaps().colorRenderable || mtlFormat.getCaps().depthRenderable;
             textureCaps.texturable        = true;
+            textureCaps.blendable         = mtlFormat.getCaps().blendable;
             textureCaps.textureAttachment = textureCaps.renderbuffer;
         }
 
