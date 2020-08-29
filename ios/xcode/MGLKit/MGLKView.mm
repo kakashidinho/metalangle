2d6f6174f4e752449ad9bc19479e17608477d3c1
Le Hoang Quyen <lehoangq@gmail.com>
2020-08-29 17:32:59 +0800

    Move setCurrentContext: and present: out of MGLKView drawRect:
    Put it inside display instead.
    
    This way, if subclass overrides drawRect: and display method is called,
    the context will still be correctly made current and its layer is
    presented at the end of the display method.

--- ios/xcode/MGLKit/MGLKView.mm
+++ ios/xcode/MGLKit/MGLKView.mm
@@ -135,7 +135,7 @@ void Throw(NSString *msg)
 
 - (void)display
 {
-    [self drawRect:self.bounds];
+    [self displayAndCapture:nullptr];
 }
 
 - (void)bindDrawable
@@ -187,10 +187,13 @@ void Throw(NSString *msg)
 
 - (void)drawRect:(CGRect)rect
 {
-    [self drawRect:rect andCapture:nullptr];
+    if (_delegate)
+    {
+        [_delegate mglkView:self drawInRect:rect];
+    }
 }
 
-- (void)drawRect:(CGRect)rect andCapture:(uint8_t **)pPixels
+- (void)displayAndCapture:(uint8_t **)pPixels
 {
     _drawing = YES;
     if (_context)
@@ -201,10 +204,7 @@ void Throw(NSString *msg)
         }
     }
 
-    if (_delegate)
-    {
-        [_delegate mglkView:self drawInRect:rect];
-    }
+    [self drawRect:self.bounds];
 
     if (pPixels)
     {
@@ -251,7 +251,7 @@ static void freeImageData(void *info, const void *data, size_t size)
 - (UIImage *)snapshot
 {
     uint8_t *pixels = nullptr;
-    [self drawRect:self.bounds andCapture:&pixels];
+    [self displayAndCapture:&pixels];
 
     if (!pixels)
     {
