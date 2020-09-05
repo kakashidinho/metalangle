c48f7570cf78d50cf1ee1128d29475a111993a73
Le Hoang Quyen <lehoangq@gmail.com>
2020-09-05 16:56:28 +0800

    Addd Transform Feedback sample code

--- ios/xcode/MGLKitSampleApp/GLViewController.m
+++ ios/xcode/MGLKitSampleApp/GLViewController.m
@@ -37,7 +37,7 @@ typedef NSImage MGLKNativeImage;
 typedef UIImage MGLKNativeImage;
 #endif  // TARGET_OS_OSX
 
-#include <MetalANGLE/GLES2/gl2.h>
+#include <MetalANGLE/GLES3/gl3.h>
 
 #import "CC3GLMatrix.h"
 
@@ -56,11 +56,23 @@ typedef UIImage MGLKNativeImage;
     GLuint _vertexBuffer;
     GLuint _indexBuffer;
     GLuint _vertexBuffer2;
-    GLuint _indexBuffer2;
+
+    // Transform Feedback
+    GLuint _xfbHandle;
+    GLuint _xfbPosBuffer;
+    GLuint _xfbColorBuffer;
+    GLuint _xfbTexcoordBuffer;
+    GLuint _xfbPassthroughVAO;
+    GLint _xfbPassthroughPosSlot;
+    GLint _xfbPassthroughColorSlot;
+    GLint _xfbPassthroughTexcoordSlot;
+    GLint _xfbPassthroughTextureUniform;
+    GLuint _xfbPassthroughProgramHandle;
 
     MGLContext *_asyncLoadContext;
 
     BOOL _resourceLoadFinish;
+    BOOL _useTransformFeedback;
 }
 
 @end
@@ -123,13 +135,15 @@ const GLubyte Indices[] = {
     20, 21, 22, 22, 23, 20};
 
 const Vertex Vertices2[] = {
+    // first triangle
     {{0.5, -0.5, 0.01}, {1, 1, 1, 1}, {1, 1}},
     {{0.5, 0.5, 0.01}, {1, 1, 1, 1}, {1, 0}},
     {{-0.5, 0.5, 0.01}, {1, 1, 1, 1}, {0, 0}},
+    // second triangle
+    {{-0.5, 0.5, 0.01}, {1, 1, 1, 1}, {0, 0}},
     {{-0.5, -0.5, 0.01}, {1, 1, 1, 1}, {0, 1}},
+    {{0.5, -0.5, 0.01}, {1, 1, 1, 1}, {1, 1}},
 };
-
-const GLubyte Indices2[] = {1, 0, 2, 3};
 
 - (void)viewDidLoad
 {
@@ -140,7 +154,21 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     // Create OpenGL context
     self.glView.drawableDepthFormat = MGLDrawableDepthFormat16;
 
-    MGLContext *context = [[MGLContext alloc] initWithAPI:kMGLRenderingAPIOpenGLES2];
+    bool useGLES3 = getenv("MGL_SAMPLE_USE_GLES3");
+
+    MGLRenderingAPI api;
+
+    if (useGLES3)
+    {
+        api                   = kMGLRenderingAPIOpenGLES3;
+        _useTransformFeedback = YES;
+    }
+    else
+    {
+        api = kMGLRenderingAPIOpenGLES2;
+    }
+
+    MGLContext *context = [[MGLContext alloc] initWithAPI:api];
     self.glView.context = context;
 
     [MGLContext setCurrentContext:context];
@@ -196,7 +224,7 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
 
 - (void)compileShaders
 {
-
+    // Normal shaders
     // 1
     GLuint vertexShader   = [self compileShader:@"SimpleVertex" withType:GL_VERTEX_SHADER];
     GLuint fragmentShader = [self compileShader:@"SimpleFragment" withType:GL_FRAGMENT_SHADER];
@@ -205,6 +233,14 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     GLuint programHandle = glCreateProgram();
     glAttachShader(programHandle, vertexShader);
     glAttachShader(programHandle, fragmentShader);
+
+    // Enable transform feedback on normal shaders.
+    if (_useTransformFeedback)
+    {
+        const GLchar *varyings[] = {"gl_Position", "DestinationColor", "TexCoordOut"};
+        glTransformFeedbackVaryings(programHandle, 3, varyings, GL_SEPARATE_ATTRIBS);
+    }
+
     glLinkProgram(programHandle);
 
     // 3
@@ -233,6 +269,46 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     _texCoordSlot = glGetAttribLocation(programHandle, "TexCoordIn");
 
     _textureUniform = glGetUniformLocation(programHandle, "Texture");
+
+    [self compileXFBShader:fragmentShader];
+}
+
+- (void)compileXFBShader:(GLuint)fragmentShader
+{
+    // Tranform feedback passthrough shader
+    if (!_useTransformFeedback)
+    {
+        return;
+    }
+
+    // Compile transform feedback playback shader.
+    GLuint xfbPassthroughVertexShader = [self compileShader:@"XFBPassthrough"
+                                                   withType:GL_VERTEX_SHADER];
+
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, xfbPassthroughVertexShader);
+    glAttachShader(programHandle, fragmentShader);
+    glLinkProgram(programHandle);
+
+    // Link
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE)
+    {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+
+    _xfbPassthroughProgramHandle = programHandle;
+
+    _xfbPassthroughPosSlot      = glGetAttribLocation(programHandle, "Position");
+    _xfbPassthroughColorSlot    = glGetAttribLocation(programHandle, "SourceColor");
+    _xfbPassthroughTexcoordSlot = glGetAttribLocation(programHandle, "TexCoordIn");
+
+    _xfbPassthroughTextureUniform = glGetUniformLocation(programHandle, "Texture");
 }
 
 - (void)setupVBOs
@@ -250,9 +326,25 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer2);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices2), Vertices2, GL_STATIC_DRAW);
 
-    glGenBuffers(1, &_indexBuffer2);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer2);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices2), Indices2, GL_STATIC_DRAW);
+    // Transform Feedback buffers for second mesh
+    if (!_useTransformFeedback)
+    {
+        return;
+    }
+
+    Vertex oneVertex;
+
+    glGenBuffers(1, &_xfbPosBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _xfbPosBuffer);
+    glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(float) * 6, NULL, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &_xfbColorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _xfbColorBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(oneVertex.Color) * 6, NULL, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &_xfbTexcoordBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _xfbTexcoordBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(oneVertex.TexCoord) * 6, NULL, GL_STATIC_DRAW);
 }
 
 - (GLuint)setupTexture:(NSString *)fileName
@@ -325,7 +417,8 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     else
     {
         // Do asynchronous resource loading
-        _asyncLoadContext = [[MGLContext alloc] initWithAPI:kMGLRenderingAPIOpenGLES2 sharegroup:self.glView.context.sharegroup];
+        _asyncLoadContext   = [[MGLContext alloc] initWithAPI:self.glView.context.API
+                                                 sharegroup:self.glView.context.sharegroup];
         __auto_type bgQueue = dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0);
         dispatch_async(bgQueue, ^{
             [MGLContext setCurrentContext:self->_asyncLoadContext];
@@ -348,15 +441,12 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
 
-    glClearColor(0, 104.0 / 255.0, 55.0 / 255.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
-
     if (!_resourceLoadFinish)
     {
         return;
     }
 
+    // Setup uniforms
     glUseProgram(_programHandle);
 
     CC3GLMatrix *projection = [CC3GLMatrix matrix];
@@ -375,13 +465,23 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     [modelView rotateBy:CC3VectorMake(_currentRotation, _currentRotation, 0)];
     glUniformMatrix4fv(_modelViewUniform, 1, 0, modelView.glMatrix);
 
+
+    if (_useTransformFeedback)
+    {
+        [self doRecordSecondMeshXFB];
+    }
+
     // 1
     glViewport(0, 0, self.size.width, self.size.height);
 
+    glClearColor(0, 104.0 / 255.0, 55.0 / 255.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+
+    // First mesh
+    // 2
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
-
-    // 2
     glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
     glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                           (GLvoid *)(sizeof(float) * 3));
@@ -396,14 +496,25 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     // 3
     glDrawElements(GL_TRIANGLES, sizeof(Indices) / sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
 
+    // Second mesh
+    if (_useTransformFeedback)
+    {
+        [self doPlaybackSecondMeshXFB];
+    }
+    else
+    {
+        [self doDrawSecondMesh];
+    }
+}
+
+- (void)doDrawSecondMesh
+{
+    // Program & Uniform matrices must already be set
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer2);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer2);
 
     glActiveTexture(GL_TEXTURE0);  // unneccc in practice
     glBindTexture(GL_TEXTURE_2D, _fishTexture);
     glUniform1i(_textureUniform, 0);  // unnecc in practice
-
-    glUniformMatrix4fv(_modelViewUniform, 1, 0, modelView.glMatrix);
 
     glEnableVertexAttribArray(_positionSlot);
     glEnableVertexAttribArray(_colorSlot);
@@ -415,7 +526,75 @@ const GLubyte Indices2[] = {1, 0, 2, 3};
     glVertexAttribPointer(_texCoordSlot, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                           (GLvoid *)(sizeof(float) * 7));
 
-    glDrawElements(GL_TRIANGLE_STRIP, sizeof(Indices2) / sizeof(Indices2[0]), GL_UNSIGNED_BYTE, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 3, 3);
+}
+
+- (void)doPlaybackSecondMeshXFB
+{
+    glUseProgram(_xfbPassthroughProgramHandle);
+
+    if (!_xfbPassthroughVAO)
+    {
+        glGenVertexArrays(1, &_xfbPassthroughVAO);
+        glBindVertexArray(_xfbPassthroughVAO);
+
+        Vertex oneVertex;
+        glBindBuffer(GL_ARRAY_BUFFER, _xfbPosBuffer);
+        glEnableVertexAttribArray(_xfbPassthroughPosSlot);
+        glVertexAttribPointer(_xfbPassthroughPosSlot, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, _xfbColorBuffer);
+        glEnableVertexAttribArray(_xfbPassthroughColorSlot);
+        glVertexAttribPointer(_xfbPassthroughColorSlot, 4, GL_FLOAT, GL_FALSE,
+                              sizeof(oneVertex.Color), 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, _xfbTexcoordBuffer);
+        glEnableVertexAttribArray(_xfbPassthroughTexcoordSlot);
+        glVertexAttribPointer(_xfbPassthroughTexcoordSlot, 2, GL_FLOAT, GL_FALSE,
+                              sizeof(oneVertex.TexCoord), 0);
+    }
+    else
+    {
+        glBindVertexArray(_xfbPassthroughVAO);
+    }
+
+    glActiveTexture(GL_TEXTURE0);  // unneccc in practice
+    glBindTexture(GL_TEXTURE_2D, _fishTexture);
+    glUniform1i(_xfbPassthroughTextureUniform, 0);  // unnecc in practice
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glBindVertexArray(0);
+}
+
+- (void)doRecordSecondMeshXFB
+{
+    glEnable(GL_RASTERIZER_DISCARD);
+
+    if (!_xfbHandle)
+    {
+        // Bind buffers for transform feedback recording
+        glGenTransformFeedbacks(1, &_xfbHandle);
+        glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, _xfbHandle);
+
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, _xfbPosBuffer);
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 1, _xfbColorBuffer);
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 2, _xfbTexcoordBuffer);
+    }
+    else
+    {
+        glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, _xfbHandle);
+    }
+
+    glBeginTransformFeedback(GL_TRIANGLES);
+
+    // Do tranform feedback on second mesh
+    [self doDrawSecondMesh];
+
+    glDisable(GL_RASTERIZER_DISCARD);
+    glEndTransformFeedback();
+    glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, 0);
 }
 
 @end
