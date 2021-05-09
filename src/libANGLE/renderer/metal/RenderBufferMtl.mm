db4c573f85c168ada570c386d9a8289cc934c632
Le Hoang Quyen <lehoangq@gmail.com>
2021-05-09 03:09:55 -0700

    Dont' use implicit MSAA by default for DS buffer anymore
    It caused DS buffer's content lost after each render pass

--- src/libANGLE/renderer/metal/RenderBufferMtl.mm
+++ src/libANGLE/renderer/metal/RenderBufferMtl.mm
@@ -75,27 +75,12 @@ angle::Result RenderbufferMtl::setStorageImpl(const gl::Context *context,
 
     if ((mTexture == nullptr || !mTexture->valid()) && (width != 0 && height != 0))
     {
-        if (actualSamples == 1 || (mFormat.hasDepthAndStencilBits() && mFormat.getCaps().resolve))
+        if (actualSamples == 1)
         {
             ANGLE_TRY(mtl::Texture::Make2DTexture(contextMtl, mFormat, static_cast<uint32_t>(width),
                                                   static_cast<uint32_t>(height), 1,
                                                   /* renderTargetOnly */ false,
                                                   /* allowFormatView */ false, &mTexture));
-
-            // Use implicit resolve for depth stencil texture whenever possible. This is because
-            // for depth stencil texture, if stencil needs to be blitted, a formatted clone has
-            // to be created. And it is expensive to clone a multisample texture.
-            if (actualSamples > 1)
-            {
-                // This format must supports implicit resolve
-                ASSERT(mFormat.getCaps().resolve);
-
-                ANGLE_TRY(mtl::Texture::Make2DMSTexture(
-                    contextMtl, mFormat, static_cast<uint32_t>(width),
-                    static_cast<uint32_t>(height), actualSamples,
-                    /* renderTargetOnly */ true,
-                    /* allowFormatView */ false, &mImplicitMSTexture));
-            }
         }
         else
         {
